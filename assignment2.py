75fa9610a211ccc6fa49888e4ed257ff26b953a9
Sepehr <Sepehrz867@deltalearns.ca>
2021-02-10 10:49:01 -0800

    done

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,5 @@
 
 #!python3
 
-print ("This is my second program")
-print("It uses \"two commands\" to display the output.")
+print ("This is my second program.")
+print ("It uses \"two commands\" to display the output.")
