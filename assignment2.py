43aa334260c3b23f73cd59d02531790432d157f5
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-02-15 14:59:12 -0800

    done

--- assignment2.py
+++ assignment2.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-print ("This is my second program")
-=======
 
 #!python3
 
 print ("This is my second program.")
->>>>>>> 75fa9610a211ccc6fa49888e4ed257ff26b953a9
 print ("It uses \"two commands\" to display the output.")
--- assignment3.py
+++ assignment3.py
@@ -3,11 +3,6 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-<<<<<<< HEAD
 print ("Hello World!")
-print ("This is how multiple print statements are joined")
-=======
-print ("Hello world!")
 print ("This is how multiple print statements are joined. Use end=""")
->>>>>>> 75fa9610a211ccc6fa49888e4ed257ff26b953a9
 
