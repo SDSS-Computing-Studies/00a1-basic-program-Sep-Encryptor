afe2f5e3142053ac2c2053b70c92c50aa3e96375
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-03-01 10:25:55 -0800

    done

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,8 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print ("Hello World!")
+print ("Hello World!", end="")
 print ("This is how multiple print statements are joined. Use end=""")
+print(' Use end=""')
+
 
